ee8f78b19756d44496e41277f91861e9a50cc865
trosbalos <62149688+trosbalos@users.noreply.github.com>
2021-11-10 14:54:03 +0300

    логика калькулятора

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -3,26 +3,8 @@ import java.util.Scanner;
 public class Calc {
     public static void main(String[] args) {
         Math math = new Math();
-        Scanner scanner = new Scanner(System.in);
-        String input;
-        System.out.println("Возможные действия ( + , - , / , * )");
-        System.out.println("Пример ввода: 10 + 10");
+        math.start();
 
-            while (!(input = scanner.nextLine()).equals("exit")) {
-
-                String[] line = input.split("\\s");
-                Integer two = Integer.valueOf(line[0]);
-                Integer three = Integer.valueOf(line[2]);
-
-                    if (line[1].equals("+")) math.addition(two, three);
-                    else if (line[1].equals("-")) math.subtraction(two, three);
-                    else if (line[1].equals("*")) math.multiply(two, three);
-                    else if (line[1].equals("/")) math.division(two, three);
-                    else System.out.println("Введен недопустимый оператор");
-
-                System.out.println("Возможные действия ( + , - , / , * )");
-                System.out.println("Пример ввода: 10 + 10");
-            }
     }
 }
 
--- src/main/java/Math.java
+++ src/main/java/Math.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Math {
 
     public Object addition(Object two, Object tree) {
@@ -20,5 +22,27 @@ public class Math {
         int i = (Integer)two - (Integer)tree;
         System.out.println("Вычитаем "+ tree + " из " + two + ". Получаем результат: " + i);
         return i;
+    }
+    public void start(){
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        System.out.println("Возможные действия ( + , - , / , * )");
+        System.out.println("Пример ввода: 10 + 10");
+
+        while (!(input = scanner.nextLine()).equals("exit")) {
+
+            String[] line = input.split("\\s");
+            Integer two = Integer.valueOf(line[0]);
+            Integer three = Integer.valueOf(line[2]);
+
+            if (line[1].equals("+")) addition(two, three);
+            else if (line[1].equals("-")) subtraction(two, three);
+            else if (line[1].equals("*")) multiply(two, three);
+            else if (line[1].equals("/")) division(two, three);
+            else System.out.println("Введен недопустимый оператор");
+
+            System.out.println("Возможные действия ( + , - , / , * )");
+            System.out.println("Пример ввода: 10 + 10");
+        }
     }
 }
